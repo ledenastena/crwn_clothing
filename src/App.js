6f2bc53f218d9fcea098952f459c88c764ee935a
Milan <kockastajabuka@gmail.com>
2020-05-06 08:35:45 +0200

    starting with router, click on menu-item takes us to the designated page (only the hats for now)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+import { Route } from 'react-router-dom';
+
+const HatPage = () => (
+  <div>
+    <h1>This is a hat page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path='/' component={HomePage} />
+      <Route path='/hats' component={HatPage} />
     </div>
   );
 }
