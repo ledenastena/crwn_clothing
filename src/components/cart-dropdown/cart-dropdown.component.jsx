e575e74d88500adb5ff9d76b6f93fa65e1798525
Milan <kockastajabuka@gmail.com>
2020-05-11 15:22:47 +0200

    now showing cart items inside of cart dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import CustomButton from '../custom-button/custom-button.component';
 import './cart-dropdown.styles.scss';
+import CartItem from '../cart-item/cart-item.component';
+import { connect } from 'react-redux';
 
-const CartDropdown = () => (
+const CartDropdown = ({ items }) => (
   <div className='cart-dropdown'>
-    <div className='cart-items'></div>
+    <div className='cart-items'>
+        { console.log(items)}
+        {items.map( item => (
+        <CartItem key={item.id} item={ item } />
+      ))}
+    </div>
     <CustomButton>CHECKOUT</CustomButton>
   </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { items }}) => ({
+  items: items
+})
+
+export default connect(mapStateToProps)(CartDropdown);
